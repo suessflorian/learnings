c3705a6bcc97f51ef1efae0d1573ba7f52899a08
Florian Suess <floriansuess96@icloud.com>
2022-02-07 08:53:46 +1300

    Simple todo, simple watch command

--- esbuild/app.jsx
+++ esbuild/app.jsx
@@ -2,8 +2,40 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const App = () => {
-  return <h1>Hi there</h1>;
+  const [todos, setTodos] = React.useState([]);
+  const [newTodo, setNewTodo] = React.useState("");
+
+  const addTodo = (newTodo) => {
+    if (newTodo in todos || newTodo == "") {
+      return;
+    }
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+      <form
+        onSubmit={(e) => {
+          addTodo(newTodo);
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          name="todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
-console.log(document.getElementById("root"))
