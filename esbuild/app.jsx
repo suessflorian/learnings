f332741fd0df5bb9482c3a971a5dc6b2a3717134
Florian Suess <floriansuess96@icloud.com>
2022-02-07 07:49:48 +1300

    Minimal esbuild example

---
+++ esbuild/app.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Hi there</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+console.log(document.getElementById("root"))
